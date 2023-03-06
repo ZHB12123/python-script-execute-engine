382afcd5ec85ad53fb1293d36566cadbcc39067a
zhanghaibin <434323746@qq.com>
2023-03-06 23:14:50 +0800

    进程隔离

--- script_execute_engine/main.py
+++ script_execute_engine/main.py
@@ -6,6 +6,7 @@ from flask import request
 import sys
 import json
 import importlib
+import multiprocessing
 
 sys.path.append("../scripts")
 #
@@ -14,19 +15,25 @@ sys.path.append("../scripts")
 app = Flask(__name__)
 
 
+def run_script(payload_dict):
+    entrance_func_name = payload_dict.get("entrance_func")
+    code = payload_dict.get("code")
+
+    exec(code, globals())
+
+    execute_func = globals()[entrance_func_name]
+    execute_result = execute_func()
+    return execute_result
+
+
 # 编写路由，构建url与函数的映射关系（将函数与url绑定）
 @app.route("/execute", methods=["POST"])
 def users():
     payload_dict = json.loads(request.data.decode("utf-8"))
 
-    entrance_func_name = payload_dict.get("entrance_func")
-    code = payload_dict.get("code")
-    with open("../scripts/code_zhb.py", "w") as fd:
-        fd.write(code)
-    module = importlib.import_module("code_zhb")
+    p = pool.apply_async(run_script, (payload_dict,))
+    execute_result = p.get()
 
-    execute_func = getattr(module, entrance_func_name, None)
-    execute_result = execute_func()
     return jsonify({"code": 10000, "message": "success", "result": execute_result})
 
 
@@ -34,10 +41,11 @@ if __name__ == '__main__':
     # 默认方式启动
     # app.run()
     # 解决jsonify中文乱码问题
+    pool = multiprocessing.Pool(processes=4)
+
     app.config['JSON_AS_ASCII'] = False
     # 以调试模式启动,host=0.0.0.0 ,则可以使用127.0.0.1、localhost以及本机ip来访问
     app.run(host="0.0.0.0", port=8899, debug=True)
-
 
 '''
 import requests
